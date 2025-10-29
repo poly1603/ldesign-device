14a2d2e4cfef0df7154bbbbd6ba7b736cd251e00
swimly <979741120@qq.com>
2025-10-29 15:55:20 +0800

    feat: 添加核心和模块的导出文件
    添加了src/core/index.ts和src/modules/index.ts文件，用于集中导出核心功能和模块功能

---
+++ src/core/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Core exports
+ */
+
+export * from './DeviceDetector'
+export * from './EventEmitter'
+export * from './ModuleLoader'
+export * from './OptimizedDeviceDetector'
+export * from './OptimizedEventEmitter'
+export * from './OptimizedModuleLoader'
---
+++ src/modules/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Modules exports
+ */
+
+export * from './BatteryModule'
+export * from './ClipboardModule'
+export * from './FeatureDetectionModule'
+export * from './GeolocationModule'
+export * from './MediaCapabilitiesModule'
+export * from './MediaModule'
+export * from './NetworkModule'
+export * from './OrientationLockModule'
+export * from './PerformanceModule'
+export * from './VibrationModule'
+export * from './WakeLockModule'
